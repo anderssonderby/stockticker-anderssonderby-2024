9a3d260481e1f1658150b9bb007f14f158bcef64
anderssonderby <50653739+anderssonderby@users.noreply.github.com>
2024-11-06 18:44:45 +0100

    Implement StockTickerClient

---
+++ StockTickerClient/Models/StockPriceModel.cs
@@ -0,0 +1,8 @@
+﻿namespace StockTickerClient.Models;
+
+public class StockPriceModel
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public DateTime Timestamp { get; set; }
+}
--- StockTickerClient/Program.cs
+++ StockTickerClient/Program.cs
@@ -1,3 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
+using StockTickerClient.Models;
 
-Console.WriteLine("Hello, World!");
+var client = new HttpClient();
+var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:5001/StockPrice/stream");
+request.Headers.Accept.ParseAdd("text/event-stream");
+
+var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+var body = await response.Content.ReadAsStreamAsync();
+var reader = new StreamReader(body);
+
+try
+{
+    while (!reader.EndOfStream)
+    {
+        var line = await reader.ReadLineAsync();
+
+        if (string.IsNullOrWhiteSpace(line))
+            continue;
+
+        if (line.StartsWith("data:"))
+        {
+            var json = line.Substring(5).Trim();
+            var stockPrice = JsonSerializer.Deserialize<StockPriceModel>(json);
+
+            Console.WriteLine($"{stockPrice.Timestamp:T} - {stockPrice.Name}: ${stockPrice.Price}");
+        }
+    }
+}
+finally
+{
+    // Release objects even if exception is thrown
+    reader.Dispose();
+    body.Dispose();
+    response.Dispose();
+    client.Dispose();
+}
